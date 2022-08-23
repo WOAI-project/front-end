078c93cf457373d25dfad4ceeb3036ae109bd393
Juuso Roinevirta <jroinevirta@gmail.com>
2022-08-23 17:46:40 +0300

    Add image data handling

--- woai/src/Components/AllTabs/ZeroView.js
+++ woai/src/Components/AllTabs/ZeroView.js
@@ -4,47 +4,57 @@ import { ethers } from "ethers";
 
 import "./ZeroView.css"
 
-//BOUNTY (1 WOAI/Zero): If the image file has not been set for the NFT you are viewing (i.e. 404), show a message telling the user so, instead of the 404 img icon
-//BOUNTY (1 WOAI/Zero): Use a (pseudo-)random NFT with existing art work as the default image when loading the tab. If there are no existing art works, show https://d2q426wzrt520c.cloudfront.net/zero/art/placeholder.png
-
 class ZeroView extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      owner: "0x0000000000000000000000000000000000000000",
-      tokenView: "p"
+      tokenOwner: "Error",
+      tokenGeneratorValue: "Not set",
+      tokenView: Math.floor(Math.random() * (2500)),
+      loadedData: false
     };
   }
 
-  //Handle reading image data 
-  //BOUNTY (1 WOAI/Zero): Show the owner of the NFT rendered under the image. Some placeholder code exists. 
-  //BOUNTY (1 WOAI/Zero): Show the generator value of the NFT loaded. Formatting is super important to make it fit into the "frame" aesthetics of the page
-  //Missing functionality - owner is not read
-  onSubmitGetOwner = async event => {
-    event.preventDefault();
 
+  // Handle image 404
+  defaultErrorImage(ev){
+    ev.target.src = "https://d2q426wzrt520c.cloudfront.net/zero/art/placeholder.png"
+  }
+  //END
+
+  // Handle reading image data 
+  getImageData = async event => {
+    this.setState({ loadedData : true })
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const woaiZeroContract = new ethers.Contract("0x9E61574ceeb8a3dd924C9f1dAE5ca95Bb684Ab29", WoaiZeroABI, provider);  //Testnet contract address
-    const tokenOwner = woaiZeroContract.ownerOf(this.state.tokenView);
-    console.log(woaiZeroContract.ownerOf(this.state.tokenView))
-
-    this.setState({ owner : tokenOwner })
-    console.log(this.state.owner, tokenOwner)
+    const tokenOwner = await woaiZeroContract.ownerOf(this.state.tokenView);
+    const tokenGeneratorValue = await woaiZeroContract.generatorValue(this.state.tokenView);
+    this.setState({ tokenOwner : tokenOwner, tokenGeneratorValue : tokenGeneratorValue })
+    console.log(this.state.tokenOwner, this.state.tokenGeneratorValue, "called2");
   }
   //END
 
 
   renderZeroView() {
+    if (!this.state.loadedData) {
+      this.getImageData()
+      return (<div className='loaderTextContainer'><p className='loaderText'>Loading...</p></div>)
+    }
     return (
       <div id="RenderZeroViewInner">
-        <img src={'https://d2q426wzrt520c.cloudfront.net/zero/art/' + this.state.tokenView + '.png'}></img> {/* Cloudfront highly availability + SSL */}
-        { /* <p>Owner: {this.state.owner.substring(0,5)}...{this.state.owner.substring(38,42)} <a href={'https://etherscan.io/address/' + this.state.owner} >↗</a></p> */}
+        <img id="imageData" onError={this.defaultErrorImage} onChange={this.getImageData} src={'https://d2q426wzrt520c.cloudfront.net/zero/art/' + this.state.tokenView + '.png'}></img> {/* Cloudfront highly availability + SSL */}
         <div>
-          <form id="viewForm" onSubmit={this.onSubmitGetOwner}>
-          <span><label>WOAI/Zero/</label><input value={this.state.tokenView} maxLength="4" onChange={event => this.setState({ tokenView: event.target.value})}></input></span>
-            { /* <button type="submit">View</button> */}
+          <form id="viewForm" onChange={this.getImageData}>
+            <span><label>WOAI/Zero/</label><input value={this.state.tokenView} maxLength="4" onChange={event => this.setState({ tokenView: event.target.value})}></input></span>
           </form>
+        </div>
+        <div id="generatorWrapper">
+          { /* <p id="generatorLabel">Generator value:</p> */ /* removed as it took away precious real estate */ }
+          <p id="generatorData">{this.state.tokenGeneratorValue}</p>
+        </div>
+        <div id="cornerOwner">
+          <p id="cornerOwnerData"><a href={'https://etherscan.io/address/' + this.state.tokenOwner} target="_blank" id="cornerOwnerEtherscan">View owner ↗</a></p>
         </div>
       </div>
     )
