83390eb91353c66dbe4379b36195d25718cb3cab
Juuso Roinevirta <jroinevirta@gmail.com>
2022-08-28 10:35:45 +0300

    Add NFT calendar verification

--- woai/src/Footer.js
+++ woai/src/Footer.js
@@ -5,6 +5,10 @@ import iconTwitter from './iconTwitter.svg';
 import iconDiscord from './iconDiscord.svg';
 import iconLooksRare from './iconLooksRare.svg';
 import iconOpenSea from './iconOpenSea.png';
+
+import iconNftCalendar from './iconNftCalendar.webp'
+import iconNonFungi from './iconNonFungi.webp'
+
 import './Footer.css';
 
 function Footer() {
@@ -13,12 +17,19 @@ function Footer() {
         <footer className="footer">
             <ul>
                 <li>
-                    <p></p>
+                    <table className="featuredTable">
+                        <tr>
+                            <th colspan="2"><p>As seen on</p></th>
+                        </tr>
+                        <tr>
+                            { /* <td><a href="https://www.non-fungi.com/" target="_blank"><img src={iconNonFungi} className="outlinks featuredIcons" title="non-fungi.com"></img></a></td> */ }
+                            <td><a href="https://nftcalendar.io/" target="_blank"><img src={iconNftCalendar} className="outlinks featuredIcons" title="nftcalendar.io"></img></a></td>
+                            <td></td>
+                        </tr>
+                    </table>
                 </li>
                 <li>
                     <center>
-                        <a href="https://docs.woai.io/woai/bounties" className="footerTextLink" target="_blank">Bounties</a>
-                        <img className="separator"></img>
                         <a href="https://github.com/WOAI-project" target="_blank"><img src={iconGithub} className="outlinks" title="Code"></img></a>
                         <a href="https://docs.woai.io/" target="_blank"><img src={iconGitbook} className="outlinks" title="Docs"></img></a>
                         <img className="separator"></img>
@@ -26,14 +37,13 @@ function Footer() {
                         <a href="https://twitter.com/woai_io" target="_blank"><img src={iconTwitter} className="outlinks" title="Twitter"></img></a>
                         <a href="https://discord.gg/76VSqqvavT" target="_blank"><img src={iconDiscord} className="outlinks" title="Discord"></img></a>
                         <img className="separator"></img>
-                        <a href="#" target="_blank"><img src={iconLooksRare} className="outlinks" title="LooksRare"></img></a>
-                        <a href="#" target="_blank"><img src={iconOpenSea} className="outlinks invert" title="OpenSea"></img></a>
-                        <img className="separator"></img>
-                        <a href="http://woai-data.woai.io/terms.html" className="footerTextLink" target="_blank">T&amp;C</a>
+                        <a href="https://looksrare.org/collections/0x0eAbED78fd49AD3c0A3e445954f29522025c09A0" target="_blank"><img src={iconLooksRare} className="outlinks" title="LooksRare"></img></a>
+                        <a href="https://opensea.io/collection/woai-zero" target="_blank"><img src={iconOpenSea} className="outlinks invert" title="OpenSea"></img></a>
+                        
                     </center>
                 </li>
                 <li>
-                    <p></p>
+                    <a href="http://woai-data.woai.io/terms.html" className="footerTextLink" target="_blank">T&amp;C</a>
                 </li>
             </ul>
 
