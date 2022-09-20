75664ac3bc5e73507f597ac959f9372f133e18d9
Juuso Roinevirta <jroinevirta@gmail.com>
2022-09-20 14:35:03 +0300

    Add discalimer on Zero use

--- zero/src/Components/Disclaimer.js
+++ zero/src/Components/Disclaimer.js
@@ -4,8 +4,8 @@ import './Disclaimer.css'
 function Disclaimer() {
   return (
     <div className="disclaimer">
-        <h1>TESTNET ONLY</h1>
-        <p>Please note that this is a testnet (Rinkeby) version only. You can familiarise yourself with the UI by connecting to Rinkeby. Some functionality may be unavailable at the moment. <br/><br/> Mainnet collection will launch on Sep 6 at 12:00 UTC.</p>
+        <h1>LIMITATION ON USE</h1>
+        <p>Please note that this contract may not be used by entities residing within the EU.</p>
     </div>
   );
 }
